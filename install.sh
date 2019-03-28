d12971ddb50651e1b501e744507365359288dd9c
rgreenblatt <greenblattryan@gmail.com>
2019-03-28 00:55:26 -0400

    updated install to install neovim depends

--- install.sh
+++ install.sh
@@ -5,14 +5,16 @@ git submodule update --recursive --remote
 mkdir -p ~/.config/
 
 if [[ "$1" != "-c" ]]; then
-   ln -s $PWD/i3 ~/.config/
-   ln -s $PWD/i3status ~/.config/
-   ln -s $PWD/keyboard ~/.config/
-   ln -s $PWD/qutebrowser ~/.config/
-   ln -s $PWD/st ~/.config/
-   ln -s $PWD/zathura ~/.config/
+    ln -s $PWD/i3 ~/.config/
+    ln -s $PWD/i3status ~/.config/
+    ln -s $PWD/keyboard ~/.config/
+    ln -s $PWD/qutebrowser ~/.config/
+    ln -s $PWD/st ~/.config/
+    ln -s $PWD/zathura ~/.config/
+    echo "let g:headless = 1" > ~/.vim_machine_specific.vim
 fi
 
+pip install neovim-remote pynvim
 ln -s $PWD/nvim ~/.config/nvim  
 if ! type "$zsh" &> /dev/null; then
     ./zimfw/install.sh
