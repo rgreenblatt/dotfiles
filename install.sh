2dc05f507f720b6bf35cd6b9ddf9d59172125e7f
rgreenblatt <greenblattryan@gmail.com>
2019-03-28 01:28:20 -0400

    fixed flip

--- install.sh
+++ install.sh
@@ -4,6 +4,9 @@ git submodule init
 git submodule update --recursive --remote
 mkdir -p ~/.config/
 
+pip install neovim-remote pynvim
+ln -s $PWD/nvim ~/.config/nvim  
+
 if [[ "$1" != "-c" ]]; then
     ln -s $PWD/i3 ~/.config/
     ln -s $PWD/i3status ~/.config/
@@ -11,13 +14,11 @@ if [[ "$1" != "-c" ]]; then
     ln -s $PWD/qutebrowser ~/.config/
     ln -s $PWD/st ~/.config/
     ln -s $PWD/zathura ~/.config/
-    echo "let g:headless = 1" > ~/.vim_machine_specific.vim
-else
     touch ~/.vim_machine_specific.vim
+else
+    echo "let g:headless = 1" > ~/.vim_machine_specific.vim
 fi
 
-pip install neovim-remote pynvim
-ln -s $PWD/nvim ~/.config/nvim  
 
 if ! type "$zsh" &> /dev/null; then
     ./zimfw/install.sh
