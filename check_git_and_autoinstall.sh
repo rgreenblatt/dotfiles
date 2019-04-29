72b97e0efd9e2d33223a958a9f257dd5104c690f
rgreenblatt <greenblattryan@gmail.com>
2019-04-29 11:22:42 -0400

    Changed output from autoinstall

--- check_git_and_autoinstall.sh
+++ check_git_and_autoinstall.sh
@@ -10,9 +10,9 @@ if [[ ! $(git status --porcelain) ]]; then
     echo "git is clean and target is defined so pulling and installing"
     git pull
     ./install.sh $(cat target)
-    nvim +PlugInstall +qa
-    nvim +PlugUpdate +qa
-    zsh -c "source ~/.zshrc && (zplug install;  zplug update)"
+    nvim +PlugInstall +qa > /dev/null
+    nvim +PlugUpdate +qa > /dev/null
+    zsh -c "source ~/.zshrc && (zplug install;  zplug update)" > /dev/null
   else 
     >&2 echo "target isn't defined"
     exit 1
