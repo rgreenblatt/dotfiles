2615177e62bd9f391a3b61d7a3d363ddcb0a6ae7
rgreenblatt <greenblattryan@gmail.com>
2019-05-11 01:51:15 -0400

    Install is todo, sub update

--- install.sh
+++ install.sh
@@ -34,6 +34,12 @@ git submodule init > /dev/null
 git submodule update --recursive --remote > /dev/null
 mkdir -p ~/.config/
 
+if [ ! -f ~/.local/share/nvim/site/autoload/plug.vim ]; then
+  curl -L -o ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+
 target=$1; shift  # Remove 'install.sh' from the argument list
 
 install_target() {
