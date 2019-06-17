bc7fbfee3a672268c62ca46987e1818c559d7006
rgreenblatt <greenblattryan@gmail.com>
2019-06-17 09:34:44 -0400

    Broke updating commands into separate script

--- autoinstall.sh
+++ autoinstall.sh
@@ -8,13 +8,7 @@ fi
 if [[ -f target ]]; then
   echo "target is defined - installing"
   ./install.sh $(cat target)
-  nvim +PlugInstall +qa
-  nvim +PlugUpdate +qa
-  nvim +PlugUpgrade +qa
-  zsh -c "mkdir -p ~/.cache && bat cache --build" > /dev/null
-  zsh -c "cd ~/.fzf && ./install --all" > /dev/null
-  zsh -c "source ~/.zshrc && zgen update;  zgen_make_save" > /dev/null
-  zsh -i -c "compinit && compinit -c" > /dev/null
+  ./run_update.sh
 else
   >&2 echo "target isn't defined"
   exit 1
