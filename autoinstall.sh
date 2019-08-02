3a32df9009bad7b09ca5b07148f7e748f06bad31
rgreenblatt <greenblattryan@gmail.com>
2019-08-02 10:33:46 -0400

    autoinstall now just provides target to install. sync pulls and runs update

--- autoinstall.sh
+++ autoinstall.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
-if [ -z "$PROFILE_SOURCED" ]; then
-  source ~/.profile
-fi
-
 if [[ -f target ]]; then
   echo "target is defined - installing"
-  git pull >/dev/null
-  git submodule init >/dev/null
-  git submodule update --recursive --remote >/dev/null
   ./install.sh $(cat target)
 else
   echo >&2 "target isn't defined"
---
+++ sync.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$PROFILE_SOURCED" ]; then
+  source ~/.profile
+fi
+
+git pull >/dev/null
+git submodule update --init --recursive --remote >/dev/null
+
+./autoinstall.sh
+./run_update.sh
