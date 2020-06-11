fbde723e10dbfc240912bef50e05d72cafedd61b
rgreenblatt <greenblattryan@gmail.com>
2020-06-11 12:28:04 -0400

    Added full path

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 CONFIG_DIR=$HOME/.config
 mkdir $CONFIG_DIR
-ln -s nvim/.config/nvim $CONFIG_DIR/nvim
-ln -s i3/.config/i3 $CONFIG_DIR/i3
-ln -s i3status/.config/i3status $CONFIG_DIR/i3status
-ln -s kitty/.config/kitty $CONFIG_DIR/kitty
+ln -s $PWD/nvim/.config/nvim $CONFIG_DIR/nvim
+ln -s $PWD/i3/.config/i3 $CONFIG_DIR/i3
+ln -s $PWD/i3status/.config/i3status $CONFIG_DIR/i3status
+ln -s $PWD/kitty/.config/kitty $CONFIG_DIR/kitty
