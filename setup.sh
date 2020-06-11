52620428c9a37322ee0a75356020d3e44579dd3d
rgreenblatt <greenblattryan@gmail.com>
2020-06-11 12:27:12 -0400

    Fix bug

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 CONFIG_DIR=$HOME/.config
 mkdir $CONFIG_DIR
 ln -s nvim/.config/nvim $CONFIG_DIR/nvim
-ln -s i3/.config/i3 $CONFIG_DIR/.config/i3
+ln -s i3/.config/i3 $CONFIG_DIR/i3
 ln -s i3status/.config/i3status $CONFIG_DIR/i3status
 ln -s kitty/.config/kitty $CONFIG_DIR/kitty
