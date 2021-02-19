ebd9f7e4d15f4848b44defd76fb8224b60027982
rgreenblatt <greenblattryan@gmail.com>
2021-02-19 13:11:29 -0500

    ranger improvements

--- ranger/.config/ranger/commands.py
+++ ranger/.config/ranger/commands.py
@@ -1,27 +1,27 @@
-from collections import deque
+import subprocess
+import os
+
+from ranger.container.file import File
 from ranger.api.commands import Command
 
-class fzf_select(Command):
-    """
-    :fzf_select
 
-    Find a file using fzf.
-
-    With a prefix argument select only directories.
-
-    See: https://github.com/junegunn/fzf
-    """
+class fzf_search(Command):
     def execute(self):
-        import subprocess
-        import os
+        if self.arg(1):
+            args = self.rest(1)
+        else:
+            args = ''
         if self.quantifier:
             # match only directories
-            command=os.environ["FZF_DIR_COMMAND"] + " | fzf +m"
+            command = os.environ["FZF_DIR_COMMAND"] + " " + args + " | fzf +m"
         else:
             # match files and directories
-            command=os.environ["FZF_DEFAULT_COMMAND"] + " | fzf +m"
-        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
-        stdout, stderr = fzf.communicate()
+            command = os.environ[
+                "FZF_DEFAULT_COMMAND"] + " " + args + " | fzf +m"
+        fzf = self.fm.execute_command(command,
+                                      universal_newlines=True,
+                                      stdout=subprocess.PIPE)
+        stdout, _ = fzf.communicate()
         if fzf.returncode == 0:
             fzf_file = os.path.abspath(stdout.rstrip('\n'))
             if os.path.isdir(fzf_file):
@@ -29,97 +29,38 @@ class fzf_select(Command):
             else:
                 self.fm.select_file(fzf_file)
 
-class fzf_rga_documents_search(Command):
-    """
-    :fzf_rga_search_documents
-    Search in PDFs, E-Books and Office documents in current directory.
-    Allowed extensions: .epub, .odt, .docx, .fb2, .ipynb, .pdf.
 
-    Usage: fzf_rga_search_documents <search string>
-    """
+class fzf_rg_general(Command):
+    @staticmethod
+    def searcher():
+        raise NotImplementedError()
+
     def execute(self):
         if self.arg(1):
             search_string = self.rest(1)
         else:
-            self.fm.notify("Usage: fzf_rga_search_documents <search string>", bad=True)
+            self.fm.notify("Usage: %s <search string>" % self.get_name(),
+                           bad=True)
             return
 
-        import subprocess
-        import os.path
-        from ranger.container.file import File
-        command="rga '%s' | fzf +m | awk -F':' '{print $1}'" % search_string
-        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
-        stdout, stderr = fzf.communicate()
+        command = (self.searcher() + " '%s' " % search_string +
+                   " | fzf +m | awk -F':' '{print $1}'")
+        fzf = self.fm.execute_command(command,
+                                      universal_newlines=True,
+                                      stdout=subprocess.PIPE)
+        stdout, _ = fzf.communicate()
         if fzf.returncode == 0:
             fzf_file = os.path.abspath(stdout.rstrip('\n'))
             self.fm.execute_file(File(fzf_file))
 
-fd_deq = deque()
-class fd_search(Command):
-    """:fd_search [-d<depth>] <query>
 
-    Executes "fd -d<depth> <query>" in the current directory and focuses the
-    first match. <depth> defaults to 1, i.e. only the contents of the current
-    directory.
-    """
+class fzf_rga(fzf_rg_general):
+    @staticmethod
+    def searcher():
+        return "rga"
 
-    def execute(self):
-        import subprocess
-        from ranger.ext.get_executables import get_executables
-        if not 'fd' in get_executables():
-            self.fm.notify("Couldn't find fd on the PATH.", bad=True)
-            return
-        if self.arg(1):
-            if self.arg(1)[:2] == '-d':
-                depth = self.arg(1)
-                target = self.rest(2)
-            else:
-                depth = '-d1'
-                target = self.rest(1)
-        else:
-            self.fm.notify(":fd_search needs a query.", bad=True)
-            return
 
-        # For convenience, change which dict is used as result_sep to change
-        # fd's behavior from splitting results by \0, which allows for newlines
-        # in your filenames to splitting results by \n, which allows for \0 in
-        # filenames.
-        null_sep = {'arg': '-0', 'split': '\0'}
-        nl_sep = {'arg': '', 'split': '\n'}
-        result_sep = null_sep
-
-        process = subprocess.Popen(['fd', result_sep['arg'], depth, target],
-                    universal_newlines=True, stdout=subprocess.PIPE)
-        (search_results, _err) = process.communicate()
-        global fd_deq
-        fd_deq = deque((self.fm.thisdir.path + os.sep + rel for rel in
-            sorted(search_results.split(result_sep['split']), key=str.lower)
-            if rel != ''))
-        if len(fd_deq) > 0:
-            self.fm.select_file(fd_deq[0])
-
-class fd_next(Command):
-    """:fd_next
-
-    Selects the next match from the last :fd_search.
-    """
-
-    def execute(self):
-        if len(fd_deq) > 1:
-            fd_deq.rotate(-1) # rotate left
-            self.fm.select_file(fd_deq[0])
-        elif len(fd_deq) == 1:
-            self.fm.select_file(fd_deq[0])
-
-class fd_prev(Command):
-    """:fd_prev
-
-    Selects the next match from the last :fd_search.
-    """
-
-    def execute(self):
-        if len(fd_deq) > 1:
-            fd_deq.rotate(1) # rotate right
-            self.fm.select_file(fd_deq[0])
-        elif len(fd_deq) == 1:
-            self.fm.select_file(fd_deq[0])
+class fzf_rg(fzf_rg_general):
+    @staticmethod
+    def searcher():
+        return "rg"
